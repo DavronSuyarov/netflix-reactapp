96bab788597dc1227483c60919d2ab6c47872fe6
Davron <shamol1302@gmail.com>
2023-08-09 19:17:43 +0900

    Netflix

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key ='574757d6c79ed5db0a6b6fc3da20c8f3'
+
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
